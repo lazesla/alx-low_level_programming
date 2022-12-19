c2692a5379dc8fdd3b20015c3e0eea914a8668a1
ESLA <you@example.com>
2022-12-19 14:03:24 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * print_array - a function that prints no element  of an array
- * @a: array name
- * @n: is the number of elements OF the array to be printed
- * Return: a and n inputs
+ * print_array - Entry point
+ * Desc: Entry
+ *@a: pointer
+ *@n: var
+ * Return: Always 0 (Success)
  */
 void print_array(int *a, int n)
 {
-	int i;
+        int i;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
-	printf("\n");
+        for (i = 0 ; i < n ; i++)
+        {
+                printf("%d", *(a + i));
+                if (i < (n - 1))
+                {
+                        printf(", ");
+                }
+        }
+        printf("\n");
 }
+
