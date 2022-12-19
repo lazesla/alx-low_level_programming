8e1241a2e78ccc73f6f3f0583084a02a29848d4f
ESLA <you@example.com>
2022-12-19 08:35:27 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,15 +7,7 @@
  * @n: input
  * Return: n
  */
-int main(void){
-	*n = 98;
 void reset_to_98(int *n)
 {
-       
-	 printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-
-}
-return (0);
+	*n = 98;
 }
