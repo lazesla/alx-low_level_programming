a6216ea04f54365114e1535a4b5a3c0c577e5718
ESLA <you@example.com>
2022-12-19 13:47:52 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_array - a function that prints n elements of an array
+ * print_array - a function that prints no element  of an array
  * @a: array name
  * @n: is the number of elements OF the array to be printed
  * Return: a and n inputs
