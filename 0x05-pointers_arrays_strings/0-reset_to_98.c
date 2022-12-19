a55b25958ba27900a22f8f85bc8094c63693de0a
ESLA <you@example.com>
2022-12-19 06:21:44 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,4 +10,17 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+        *n = 98;
+}
 }
