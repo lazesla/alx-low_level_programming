92ed26d42464bfe98e146c818455c934922f881a
ESLA <you@example.com>
2022-12-20 18:00:37 -0800

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
-/**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	return (0);
+
+int main()
+	{
+	int i, j, num, isPrime;
+
+    /* Input a number from user */
+	scanf("%d", &612852475143);
+
+	printf("All Prime Factors of %d are: \n", 612852475143);
+
+    /* Find all Prime factors */
+	for(i=2; i<=612852475143; i++)
+	{
+        /* Check 'i' for factor of num */
+	if(612852475143%i==0)
+	{
+            /* Check 'i' for Prime */
+            isPrime = 1;
+		for(j=2; j<=i/2; j++)
+		{
+		if(i%j==0)
+		{
+		isPrime = 0;
+		break;
+		}
+	}
+
+            /* If 'i' is Prime number and factor of num */
+		if(isPrime==1)
+		{
+		printf("("%d, ", i);
+	}
+	}
+	}
+
+    return 0;
 }
+
+
