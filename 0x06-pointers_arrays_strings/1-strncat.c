ae6cc3d32c3e5108f89fa700d5cffaef636479e4
ESLA <you@example.com>
2022-12-21 12:14:07 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int count = 0;
+
+	while (*dest++)
+		;
+	dest--;
+	while (*src && count < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		count++;
+	}
+	*dest = '\0';
+	return (p);
+}
