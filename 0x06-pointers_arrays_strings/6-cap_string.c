f02cb64ee52f52934f2a57f7614611d449230e26
ESLA <you@example.com>
2022-12-22 11:35:09 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalize all words of a string
+ * @s: string with words to capitalize
+ * Return: pointer to resulting string
+ */
+char *cap_string(char *s)
+{
+	char *r = s;
+	int i;
+	char a[] = " \t\n,.!?\"(){}";
+	int cap = 1;
+
+	while (*s)
+	{
+		if (cap && *s >= 'a' && *s <= 'z')
+			*s -= 32;
+		cap = 0;
+		for (i = 0; i < 12; i++)
+		{
+			if (*s == a[i])
+				cap = 1;
+		}
+		s++;
+	}
+	return (r);
+}
