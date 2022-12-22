3769cc82c751899c18b6093b2d5bb8d92b4a5600
ESLA <you@example.com>
2022-12-22 11:48:16 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to concate with src
+ * @src: string to concate to dest
+ * Return: pointer to dest
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *p = dest;
