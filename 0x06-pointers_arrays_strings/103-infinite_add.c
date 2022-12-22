dd61f0bc7465bf98cebe1cc3fef1475b89e72bce
ESLA <you@example.com>
2022-12-22 12:11:26 -0800

    task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * infinite_add - add two string numbers
+ * @n1: string number to add to n2
+ * @n2: string number to add to n1
+ * @r: buffer to store the sum
+ * @size_r: size of buffer
+ * return: pointer to sum or 0 if it does not fit in r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, lenS = 0;
+	
+	while (*(n1 + len1++))
+		;
+	while (*(n2 + len2++))
+		;
+	printf("%d, %d\n", len1, len2);
+	return (r);
+}
