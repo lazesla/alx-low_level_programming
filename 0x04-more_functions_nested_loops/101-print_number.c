0ba561d7b2472240e1d84e4731ceaf0dd2cb5813
ESLA <you@example.com>
2022-12-23 12:27:37 -0800

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,28 @@
 #include "main.h"
+
 /**
- * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
+ * print_number - Prints an integer
+ * @n: integer to print
  */
 void print_number(int n)
 {
+	int lastDigit;
 
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		lastDigit = n % 10 * -1;
+		n = n / -10;
+		if (n > 0)
+			print_number(n);
+		_putchar('0' + lastDigit);
+	}
+	else if (n < 10)
+		_putchar('0' + n % 10);
+	else
+	{
+		print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
 }
+Footer
