652528bb6a38291a62cb1bd2c74fbb28160bc54f
ESLA LAZARUS <eslalazarus23@gmail.com>
2022-12-28 23:02:31 -0800

    task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls fibonacci
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
+
+/**
+ * fibonacci - print first 100 fibonacci numbers
+ *
+ * Return: void
+ */
+void fibonacci(void)
+{
+	int i;
+	unsigned long a, b, c;
+
+	for (a = 1, b = 2, i = 0; i < 50; i++)
+	{
+		printf("%lu", a);
+		if (i < 49)
+			printf(", ");
+		c = a;
+		a = b;
+		b += c;
+	}
+	puts("");
+}
