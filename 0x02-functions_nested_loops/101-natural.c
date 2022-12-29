5f1bebf595febebe20ba223e3f77cd3b0bb83f28
ESLA LAZARUS <eslalazarus23@gmail.com>
2022-12-28 23:00:02 -0800

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - calls natural
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-
+	natural();
 	return (0);
+}
+
+/**
+ * natural - prints sum of all multiples of 3 & 5 less thn 1024
+ *
+ * Return: void
+ */
+void natural(void)
+{
+	int i, s;
+
+	for (i = 0, s = 0; i < 1024; i++)
+	{
+		if (!(i % 3) || !(i % 5))
+			s += i;
+	}
+	printf("%d\n", s);
 }
