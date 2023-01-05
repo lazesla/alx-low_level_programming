cbe1b960df94f222520565ab94a6029b4913ebbb
ESLA LAZARUS <eslalazarus23@gmail.com>
2023-01-04 21:55:40 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,32 @@
 #include "main.h"
-#include <stdio.h>
-int squareroot(int n, int i);
+#include <stddef.h>
+
 /**
- * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
- * @n: input
- * Return: Always 0 (Success)
+ * do_sqrt_recursion - needs the extra parameter r to work
+ * @n: the natural number whose root to find
+ * @r: the root iterator
+ *
+ * Return: the natural root of n or -1 if not found
+ */
+int do_sqrt_recursion(int n, int r)
+{
+	if (r * r == n)
+		return (r);
+	else if (r * r > n)
+		return (-1);
+	return (do_sqrt_recursion(n, r + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the square root of x
+ * @n: the number to root
+ *
+ * Return: the integer result, -1 on error
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
-		return (squareroot(n, (n + 1) / 2));
-}
-/**
- * squareroot - checks if perfect square
- * @n: input
- * @i: counter
- * Return: if square root
- */
-int squareroot(int n, int i)
-{
-	if (i < 1)
-		return (-1);
-	else if (i * i == n)
-		return (i);
-	else
-		return (squareroot(n, i - 1));
+
+	return (do_sqrt_recursion(n, 0));
 }
