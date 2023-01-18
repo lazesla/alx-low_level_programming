66ca2801dd1cbd41563eb31541eac4f291b1dff1
ESLA LAZARUS <eslalazarus23@gmail.com>
2023-01-18 02:06:43 -0800

    task o

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_name - prints the name
+ * @name: name
+ * @f: function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
