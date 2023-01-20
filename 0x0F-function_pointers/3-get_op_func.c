706f854588321bcc8d79112a69759a58eb63860c
ESLA LAZARUS <eslalazarus23@gmail.com>
2023-01-19 20:40:25 -0800

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,53 +1,29 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
- * op_add - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_add(int a, int b)
+* get_op_func - returns pointer to right op func
+* @s: string operator
+*
+* Return: the right function pointer
+*/
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
- * op_sub - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0] && !s[1])
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
