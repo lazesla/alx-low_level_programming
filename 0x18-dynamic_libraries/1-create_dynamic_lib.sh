f7c58de01eaa079ff1fd114f2ca096932d7cf334
lazesla <eslalazarus23@gmail.com>
2023-03-20 11:28:01 -0700

    Task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC *.C
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
