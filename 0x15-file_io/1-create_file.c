d05014eff6b06198a8c84ddd04753858ff9f600f
ESLA LAZARUS <eslalazarus23@.com>
2023-04-25 02:39:06 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+
 /**
- * create_file - Entry Point
- * @filename: file name
- * @text_content: null terminated string to write
- * Return: 1
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
+	write(file, text_content, i);
 
-	if (text_content == NULL)
-	{
-		close(file);
-		return (-1);
-	}
-	else
-	{
-		write(file, text_content, i);
-	}
-
-	close(file);
 	return (1);
 }
